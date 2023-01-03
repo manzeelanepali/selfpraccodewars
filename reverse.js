a891c57d2e5eaef065560c7b20b4c8141bc85cad
manzeelanepali <nepalimanzeela@gmail.com>
2023-01-03 13:11:38 +0545

    feat : reversing the array using reverse method

--- reverse.js
+++ reverse.js
@@ -1,11 +1,9 @@
-// complete the function that accepts the string parameter and reverse each word in string. All spaces in the string should be retained 
+// complete the function that accepts the string parameter and reverse each word in string. All spaces in the string should be retained
 
 // eg: " This is an example!"=> "siht si na !elpmaxe"
 // eg : "Dobule Spaces "=> "elbuod secaps"
 
-// pseudocode 
-
-function reverseArr (string ){
-    return string.push
-}
-
+// pseudocode
+const numbers = [1, 2, 3, 4];
+let rev = numbers.reverse();
+console.log(rev);
