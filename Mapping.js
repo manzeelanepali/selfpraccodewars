cf68a5351c959c62f88282cb4adbc904e4c9ca36
manzeelanepali <nepalimanzeela@gmail.com>
2023-01-14 19:14:58 +0545

    feat : using mapping method and excesing all the elements of the array and converting it to the upper case

--- Mapping.js
+++ Mapping.js
@@ -1,7 +1,13 @@
-const numbers = [1, 2, 3, 4, 5];
+// const numbers = [1, 2, 3, 4, 5];
 
-const doubleNumbers = numbers.map(function (number) {
-  return number * 2;
-});
+// const doubleNumbers = numbers.map(function (number) {
+//   return number * 2;
+// });
 
-console.log(doubleNumbers);
+// console.log(doubleNumbers);
+
+const words = ["apple", "banana", "orange"];
+
+const upperWords = words.map((word) => word.toUpperCase());
+
+console.log(upperWords);
