7136771d998f2ba3b04c4f736b410300ee1c19eb
manzeelanepali <nepalimanzeela@gmail.com>
2023-01-14 19:10:01 +0545

    feat : use of the mapping function

---
+++ Mapping.js
@@ -0,0 +1,7 @@
+const numbers = [1, 2, 3, 4, 5];
+
+const doubleNumbers = numbers.map(function (number) {
+  return number * 2;
+});
+
+console.log(doubleNumbers);
--- reverse.js
+++ reverse.js
@@ -3,10 +3,32 @@
 // eg: " This is an example!"=> "siht si na !elpmaxe"
 // eg : "Dobule Spaces "=> "elbuod secaps"
 
-// pseudocode
-let array = [8, 2, 3, 4];
-let revaray = [...array].reverse();
+// let array = [8, 2, 3, 4];
+// let revaray = [...array].reverse();
 
-// for finding the reverse by creating the shallow copy
+// console.log(revaray);
 
-console.log(revaray);
+// function reverseString(str) {
+//   // empty string
+//   let newString = "";
+//   for (let i = str.length - 1; i >= 0; i--) {
+//     newString += str[i];
+//   }
+//   return newString;
+// }
+// console.log("new string ", newString());
+
+// take input from the user
+// const string = prompt("Enter a string: ");
+
+// const result = reverseString(string);
+// console.log(result);
+
+function reverse(string) {
+  let newString = "";
+  for (let i = 0; i > string.length - 1; i--) {
+    let currVal = string[i];
+  }
+  return newString;
+}
+console.log(reverse("this is a frog"));
