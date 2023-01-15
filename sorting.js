7e343276d27fd6adc600f3c193e4e3440edac8b0
manzeelanepali <nepalimanzeela@gmail.com>
2023-01-15 21:47:26 +0545

    feat : arrow function that uses sorting methods

--- sorting.js
+++ sorting.js
@@ -1,5 +1,9 @@
+// let numbers = [4, 2, 5, 1, 3];
+// numbers.sort(function (a, b) {
+//   return a - b;
+// });
+// console.log(numbers);
+
 let numbers = [4, 2, 5, 1, 3];
-numbers.sort(function (a, b) {
-  return a - b;
-});
+numbers.sort((a, b) => a - b);
 console.log(numbers);
