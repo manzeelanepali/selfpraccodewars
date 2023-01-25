b266813981688db47a754c1520fe2fa7fd95a36f
manzeelanepali <nepalimanzeela@gmail.com>
2023-01-25 12:40:37 +0545

    use of the find method

--- filter.js
+++ filter.js
@@ -12,17 +12,31 @@
 
 // console.log(filteredNumbers);
 
-const myArray = [
-  -7, -5, -2, 2, 1, 3, 12, 14, 13, 15, 70, 17, 33, 25, 27, 30, 97,
-];
+// const myArray = [
+//   -7, -5, -2, 2, 1, 3, 12, 14, 13, 15, 70, 17, 33, 25, 27, 30, 97,
+// ];
 
-const primeNumbers = myArray.filter((element) => {
-  for (let i = 2; element > i; i++) {
-    if (element % i === 0) {
-      return false;
-    }
-  }
-  return element > 1;
+// const primeNumbers = myArray.filter((element) => {
+//   for (let i = 2; element > i; i++) {
+//     if (element % i === 0) {
+//       return false;
+//     }
+//   }
+//   return element > 1;
+// });
+
+// console.log(primeNumbers);
+
+// const delight = menu.find((menu) => menu.name === "country delight");
+
+// if (delight.status !== "86") {
+//   console.log("Available!");
+// } else {
+//   console.log("Sorry, the item is not available :(");
+// }
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let firstEvenNumber = numbers.find(function (number) {
+  return number % 2 == 0;
 });
-
-console.log(primeNumbers);
+console.log(firstEvenNumber);
