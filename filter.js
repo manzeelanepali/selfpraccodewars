c9968bcac4d146e39ad68f4b7bf3baf11c791f58
manzeelanepali <nepalimanzeela@gmail.com>
2023-01-25 12:33:39 +0545

    filter method

--- filter.js
+++ filter.js
@@ -1,5 +1,12 @@
+// const numbers = [20, 40, 17, 99, 59, 77];
+// const filteredNumbers = numbers.filter(function (number) {
+//   return number > 20;
+// });
+
+// console.log(filteredNumbers);
+
 const numbers = [20, 40, 17, 99, 59, 77];
-const filteredNumbers = numbers.filter(function (number) {
+const filteredNumbers = numbers.filter((number) => {
   return number > 20;
 });
 
