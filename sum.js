4c2d4d0954a2e00e7aae12c061197982edd44ef8
manzeelanepali <nepalimanzeela@gmail.com>
2023-03-13 22:21:25 +0545

    normal sum

--- sum.js
+++ sum.js
@@ -6,20 +6,26 @@
 // 1. go through the list of arrays
 // 2. sum every element of the array to one another and if the value matches to 9 . return the pair
 
-function functionaname(array, sum) {
-  let newarr = [];
+function findingSum(array) {
+  let sum = 0;
   for (let i = 0; i < array.length; i++) {
     let currVal = array[i];
-
-    for (let j = 0; j < array.length; j++) {
-      let secondLoop = array[j];
-
-      if (array[i] + array[j] === sum) {
-        newarr.push([array[i], array[j]]);
-      }
-    }
+    sum += currVal;
   }
-  return newarr;
+  return sum;
 }
 
-console.log(functionaname([1, 2, 3, 4, 5, 6, 7], 9));
+console.log(findingSum([1, 2, 3, 4, 5, 6, 7]));
+
+//   for (let i = 0; i < array.length; i++) {
+//     let currVal = array[i];
+//     console.log(currVal, "masala ");
+//   }
+//   for (let j = 0; j < array.length; j++) {
+//     let secondCurrVal = array[j];
+//     console.log(secondCurrVal, "dukha na dey");
+//   }
+//   // if (sum === array[i] + array[j]) {
+//   //   return array[i], array[j];
+//   // }
+// }
