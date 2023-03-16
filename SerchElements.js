ed345ad8d4325985c809be02c30722bc8cc6f731
manzeelanepali <nepalimanzeela@gmail.com>
2023-03-16 23:37:37 +0545

    find method for finding elements in the arrary

--- SerchElements.js
+++ SerchElements.js
@@ -1,9 +1,14 @@
-const students = [
-  { name: "Alice", grade: 90 },
-  { name: "Bob", grade: 80 },
-  { name: "Charlie", grade: 95 },
-];
+// const students = [
+//   { name: "Alice", grade: 90 },
+//   { name: "Bob", grade: 80 },
+//   { name: "Charlie", grade: 95 },
+// ];
 
-const topStudent = students.find((student) => student.grade >= 90);
+// const topStudent = students.find((student) => student.grade >= 90);
 
-console.log(topStudent);
+// console.log(topStudent);
+
+const fruits = ["banana", "apple", "orange", "grape"];
+
+console.log(fruits.includes("apple")); // Output: true
+console.log(fruits.includes("pear"));
