d21c1a9057d0aa944a2630423d9ff2d4962e8b55
manzeelanepali <nepalimanzeela@gmail.com>
2023-03-16 23:28:51 +0545

    feat : an example of slice

--- DeletingElement.js
+++ DeletingElement.js
@@ -1,6 +1,12 @@
-const numbers = [1, 2, 3, 4, 5];
+// const numbers = [1, 2, 3, 4, 5];
 
-// Remove the element at index 2
-numbers.splice(2, 1);
+// // Remove the element at index 2
+// numbers.splice(2, 1);
 
-console.log(numbers); // Output: [1, 2, 4, 5]
+// console.log(numbers); // Output: [1, 2, 4, 5]
+
+const letters = ["a", "b", "c", "d", "e"];
+
+const slicedLetters = letters.slice(1, 4);
+
+console.log(slicedLetters); // Output: ['b', 'c', 'd']
