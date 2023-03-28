0df3508c912dff556ade91c8bd94d09d1c0a4813
manzeelanepali <nepalimanzeela@gmail.com>
2023-03-28 21:16:11 +0545

    vowels and consonent finding

--- consonentVoel.js
+++ consonentVoel.js
@@ -1,3 +1,12 @@
+let string = "helloWorld";
+
+function consonent(string) {
+  let vowels = string.match(/[aeiou]/gi);
+  console.log("vowels", vowels);
+  let consonentHere = string.match(/[^aeiou]/gi);
+}
+console.log(consonent(string));
+
 // const string = "Hello, World!";
 // let vowels = 0;
 // let consonants = 0;
@@ -6,7 +15,7 @@
 //   const char = string[i].toLowerCase();
 //   if ("aeiou".includes(char)) {
 //     vowels++;
-//   } else if (char >= "a" && char <= "z") {
+//   } else if {
 //     consonants++;
 //   }
 // }
@@ -14,9 +23,9 @@
 // console.log("Vowels:", vowels);
 // console.log("Consonants:", consonants);
 
-const str = "Hello World";
-const vowels = str.match(/[aeiou]/gi);
-const consonants = str.match(/[^aeiou\s]/gi);
+// const str = "Hello World";
+// const vowels = str.match(/[aeiou]/gi);
+// const consonants = str.match(/[^aeiou]/gi);
 
-console.log("Vowels:", vowels); // Output: ["e", "o", "o"]
-console.log("Consonants:", consonants); // Output: ["H", "l", "l", "W", "r", "l", "d"]
+// console.log("Vowels:", vowels); // Output: ["e", "o", "o"]
+// console.log("Consonants:", consonants); // Output: ["H", "l", "l", "W", "r", "l", "d"]
