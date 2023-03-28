c63bf2607fd7542535e908063058deec6037515c
manzeelanepali <nepalimanzeela@gmail.com>
2023-03-28 20:06:04 +0545

    sort method for finding the sum of two lowest numbers in the array

--- acending.js
+++ acending.js
@@ -5,20 +5,29 @@
 // [10, 343445353, 3453445, 3453545353453] should return 3453455.
 
 // go through the list of arrays
-// Arrange the list of elements in the ascending order 
+// Arrange the list of elements in the ascending order
 // find the sum of first two elwments in the array
 // return sum
 
-function twoHighest(arr) {
-    res = arr.sort((a, b) => a - b);
-    // console.log(res);
-    let sum = res[0] + res[1]
-  
-  
-  
-  
-    return sum;
-  
-  }
-  
-  console.log(twoHighest([4, 55, 65, 64]))
+// function twoHighest(arr) {
+//     res = arr.sort((a, b) => a - b);
+//     // console.log(res);
+//     let sum = res[0] + res[1]
+
+//     return sum;
+
+//   }
+
+//   console.log(twoHighest([4, 55, 65, 64]))
+
+let numbers = [19, 5, 42, 2, 77];
+
+function finding(numbers) {
+  let finalResult = [];
+  result = numbers.sort((a, b) => a - b);
+  console.log(result);
+  finalResult.push(result[0] + result[1]);
+
+  return finalResult;
+}
+console.log(finding(numbers));
